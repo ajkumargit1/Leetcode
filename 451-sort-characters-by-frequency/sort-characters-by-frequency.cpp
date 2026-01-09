821c65cbf48051a9468bbcd497c6530b94e34f49
ajkumargit1 <ajaykumarvns2004@gmail.com>
2026-01-09 23:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (38.66%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool cmp(const pair<char, int>& a, const pair<char, int>& b) {
+        return a.second > b.second;
+    }
+
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        vector<pair<char, int>> v(freq.begin(), freq.end());
+        sort(v.begin(), v.end(), cmp);
+
+        string result = "";
+        for (auto &p : v) {
+            result.append(p.second, p.first);
+        }
+        return result;
+    }
+};
