80ad64616b40b6c82c9f3fb34467f4ad7a347669
Ajay Kumar <ajaykumarvns2004@gmail.com>
2026-09-04 20:04:42 +0530

    Time: 1 ms (83.90%) | Memory: 12 MB (94.61%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -4,22 +4,27 @@ public:
         int n = s.size();
         if (n == 0) return "";
         
-        long long fh = 0;
-        long long bh = 0;
-        long long pwr = 1;
-        long long mod = 1e9 + 7;
+        long long fh1 = 0, bh1 = 0, pwr1 = 1;
+        long long mod1 = 1e9 + 7;
+        int base1 = 29;
         
-        int base = 29;
+        long long fh2 = 0, bh2 = 0, pwr2 = 1;
+        long long mod2 = 1e9 + 9; 
+        int base2 = 31;
+        
         int max_len = 0;
-        
         for (int i = 0; i < n; i++) {
             int val = s[i] - 'a' + 1;
             
-            fh = (fh * base + val) % mod;
-            bh = (bh + val * pwr) % mod;
-            pwr = (pwr * base) % mod;
+            fh1 = (fh1 * base1 + val) % mod1;
+            bh1 = (bh1 + val * pwr1) % mod1;
+            pwr1 = (pwr1 * base1) % mod1;
             
-            if (fh == bh) {
+            fh2 = (fh2 * base2 + val) % mod2;
+            bh2 = (bh2 + val * pwr2) % mod2;
+            pwr2 = (pwr2 * base2) % mod2;
+            
+            if (fh1 == bh1 && fh2 == bh2) {
                 max_len = i + 1;
             }
         }
